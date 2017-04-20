0f7550f168b0f4301b600e2cdd7f9070d6eec0b8
sagethedawg <sagethedawg@gmail.com>
2017-04-20 13:41:42 -0700

    Add files via upload

---
+++ RGBW-16-for-loop.ino/RGBW-16-for-loop.ino.ino
@@ -0,0 +1,151 @@
+// RGBW (Red Green Blue White Neo-Pixel starter code
+// 16 LEDS  increment
+// CW Coleman 170413
+int clearColor();
+int redColor();
+int blueColor();
+int greenColor();
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+#define NUM_LEDS 16
+
+#define BRIGHTNESS 25
+#define BRIGHTNESS1 100
+#define BRIGHTNESS2 150
+#define BRIGHTNESS3 255
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(115800);
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+void loop() { 
+  clearColor(); redColor(); 
+  blueColor(); greenColor();
+  clearColor(); blueColor(); 
+  redColor(); greenColor(); 
+  clearColor();greenColor(); 
+  blueColor(); redColor(); 
+}
+
+int clearColor(){
+  strip.setBrightness(BRIGHTNESS);
+  // Initialize some variables for the void loop()
+int red = 0, green= 0, blue = 0, white = 100;
+int wait = 800;
+int led = 0;
+int i;
+
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 32;
+    green = green + 25;
+    blue = blue + 47;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+}
+int redColor(){
+  strip.setBrightness(BRIGHTNESS1);
+  // Initialize some variables for the void loop()
+int red = 255, green= 54, blue = 54, white = 0;
+int wait = 800;
+int led = 0;
+int i;
+
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 32;
+    green = green + 25;
+    blue = blue + 47;
+    white = white + 1;
+
+    if (red > 255) red = 255;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+}
+int blueColor(){
+  strip.setBrightness(BRIGHTNESS2);
+  // Initialize some variables for the void loop()
+int red = 54, green= 54, blue = 255, white = 0;
+int wait = 800;
+int led = 0;
+int i;
+
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 32;
+    green = green + 25;
+    blue = blue + 47;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 255;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+}
+int greenColor(){
+  strip.setBrightness(BRIGHTNESS3);
+  // Initialize some variables for the void loop()
+int red = 54, green= 255, blue = 54, white = 0;
+int wait = 800;
+int led = 0;
+int i;
+
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 32;
+    green = green + 25;
+    blue = blue + 47;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 255;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+}
---
+++ RGBW-16-static.ino.ino/RGBW-16-static.ino.ino.ino
@@ -0,0 +1,115 @@
+// RGBW (Red Green Blue White Neo-Pixel starter code
+// 16 LEDS with no loop inside of void loop()
+// CW Coleman 170413
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+#define NUM_LEDS 16
+
+#define BRIGHTNESS 50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(115200);
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+// Initialize some variables for the void loop()
+int led, red, green, blue, white;
+int wait = 0;
+
+void loop() {
+
+  led = 0; red = 255; green = 0; blue = 0; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+  strip.show();
+  
+  led = 1; red = 0; green = 255; blue = 8; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+  strip.show();
+     
+  led = 2; red = 0; green = 0; blue = 255; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+  strip.show();
+  
+  led = 3; red = 55; green = 255; blue = 118; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+  strip.show();
+  
+  led = 4; red = 110; green = 255; blue = 25; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+
+  led = 5; red = 65; green = 16; blue = 255; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+ 
+  led = 6; red = 255; green = 89; blue = 67; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+
+    led = 7; red = 99; green = 255; blue = 54; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+
+    led = 8; red = 65; green = 125; blue = 255; white = 0;
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+
+    led = 9; red = 255; green = 161; blue = 89; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+
+    led = 10; red = 101; green = 255; blue = 88; white = 0;
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+
+    led = 11; red = 210; green = 154; blue = 255; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+   
+    led = 12; red = 255; green = 126; blue = 38; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+   
+    led = 13; red = 10; green = 255; blue = 48; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+   strip.show();
+   
+   led = 14; red = 20; green = 50; blue = 255; white = 0;
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);  
+   strip.show();
+  
+  led = 15; red = 255; green = 126; blue = 5; white = 0; 
+  strip.setPixelColor(led, red, green , blue, white);
+  delay(wait);
+  strip.show();
+   
+   
+}
+
+
